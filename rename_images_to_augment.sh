74c86e64389eacaaecd82d3dd086ebc65640909f
alperk <alper.kucukkomurler@stm.com.tr>
2018-07-11 19:48:47 +0300

    changes to augmentation script

--- rename_images_to_augment.sh
+++ rename_images_to_augment.sh
@@ -5,3 +5,8 @@ mkdir road
 cd gt_augmented_image
 for i in *_lane_*.png; do cp "$i" "../lane/${i/_lane/}"; done;
 for i in *_road_*.png; do cp "$i" "../road/${i/_road/}"; done;
+cd ..
+mkdir augmented_image
+mkdir gt_augmented_image
+mv image_2/output/_* gt_augmented_image
+mv image_2/output/* augmented_image
